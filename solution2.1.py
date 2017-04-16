888415dc242865eeea20fb8c38675ef21cf3c6d1
jacob zlogar <jacob.zlogar@gmail.com>
2017-04-16 17:23:24 -0400

    passing solution for problem 2

--- solution2.1.py
+++ solution2.1.py
@@ -2,30 +2,39 @@ def answer(l, t):
 
     result = []
     target = int(t)
-
+    
     for key, value in enumerate(l):
+        # first loop, use the whole list here
         if key == 0:
-            slice = l[:]
-            for index, value in enumerate(slice):
-                output = slice[:index+1]
-                if sum(output) == target:
-                    result.append(output)
+            # loop list n times where n = length of the list
+            for index, value in enumerate(l):
+                slice = l[:index+1]
+                # if the sum of the slice is equal to our target append index values to result and return
+                if sum(slice) == target:
+                    length = len(slice)
+                    result.append(key)
+                    result.append(key + length - 1)
+                    return result
+                # otherwise break out of the loop
+                elif sum(slice) > target:
+                    break
+                    
+        # slice the list from the key to the end
         else:
-            slice = l[key:]
-            for index, value in enumerate(slice):
-                output = slice[:index+1]
-                if sum(output) == target:
-                    result.append(output)
+            list_slice = l[key:]
+            # loop list n times where n = length of the list_slice
+            for index, value in enumerate(list_slice):
+                slice = list_slice[:index+1]
+                if sum(slice) == target:
+                    length = len(slice)
+                    result.append(key)
+                    result.append(key + length - 1)
+                    return result
+                elif sum(slice) > target:
+                    break
 
-    if result:
-        index = []
-        for x in result:
-            for y in x:
-                index.append(l.index(y))
+    if not result:
+        index = [-1, -1]
         return index
-    else:
-        index = [-1,-1]
-        return index
 
-answer([4, 3, 10, 2, 8],12)
-
+print(answer([1, 3, 1, 2, 15, 3], 15))
