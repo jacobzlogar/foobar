e73d17b89ad8b8584facb0e3d2baaf510a2fcf0a
jacob zlogar <jacob.zlogar@gmail.com>
2017-04-20 13:29:31 -0400

    bomb_baby not passing yet

--- bomb_baby.py
+++ bomb_baby.py
@@ -7,13 +7,33 @@
 # Every Mach bomb retrieves a sync unit from a Facula bomb; for every Mach bomb, a Facula bomb is created;
 # Every Facula bomb spontaneously creates a Mach bomb.
 # 
-# For example, if you had 3 Mach bombs and 2 Facula bombs, they could either produce 3 Mach bombs and 5 Facula bombs, or 5 Mach bombs and 2 Facula bombs. The replication process can be changed each cycle. 
+# For example, if you had 3 Mach bombs and 2 Facula bombs, they could either produce 
+# 3 Mach bombs and 5 Facula bombs, or 5 Mach bombs and 2 Facula bombs. 
+# The replication process can be changed each cycle. 
 # 
-# Second, you need to ensure that you have exactly the right number of Mach and Facula bombs to destroy the LAMBCHOP device. Too few, and the device might survive. Too many, and you might overload the mass capacitors and create a singularity at the heart of the space station - not good! 
+# Second, you need to ensure that you have exactly the right number
+# of Mach and Facula bombs to destroy the LAMBCHOP device.
+# Too few, and the device might survive.
+# Too many, and you might overload the mass capacitors 
+# and create a singularity at the heart of the space station - not good! 
 # 
-# And finally, you were only able to smuggle one of each type of bomb - one Mach, one Facula - aboard the ship when you arrived, so that's all you have to start with. (Thus it may be impossible to deploy the bombs to destroy the LAMBCHOP, but that's not going to stop you from trying!) 
+# And finally, you were only able to smuggle one of each type of bomb - one Mach, one Facula - 
+# aboard the ship when you arrived, so that's all you have to start with. 
+# (Thus it may be impossible to deploy the bombs to destroy the LAMBCHOP, 
+# but that's not going to stop you from trying!) 
 # 
-# You need to know how many replication cycles (generations) it will take to generate the correct amount of bombs to destroy the LAMBCHOP. Write a function answer(M, F) where M and F are the number of Mach and Facula bombs needed. Return the fewest number of generations (as a string) that need to pass before you'll have the exact number of bombs necessary to destroy the LAMBCHOP, or the string "impossible" if this can't be done! M and F will be string representations of positive integers no larger than 10^50. For example, if M = "2" and F = "1", one generation would need to pass, so the answer would be "1". However, if M = "2" and F = "4", it would not be possible.
+# You need to know how many replication cycles (generations) it will take to generate the correct amount 
+# of bombs to destroy the LAMBCHOP.
+# Write a function answer(M, F) where M and F are the number of Mach and Facula bombs needed. 
+# Return the fewest number of generations (as a string) that need to pass 
+# before you'll have the exact number of bombs necessary to destroy the LAMBCHOP, 
+# or the string "impossible" if this can't be done!
+# M and F will be string representations of positive integers no larger than 10^50.
+# For example, if M = "2" and F = "1", one generation would need to pass,
+# so the answer would be "1". However, if M = "2" and F = "4", it would not be possible.
+# 
+# 
+# 
 # Test cases
 # ==========
 # 
@@ -29,34 +49,45 @@
 # Output:
 #     (string) "4"
 # 
-# needs optimization and more edge case tests i.e. big big ints
-# passes 2/5 tests #3, #5
+
 def answer(M, F):
+
     mach = int(M)
     facula = int(F)
 
-    if facula > mach:
-        facula = facula - (mach * round(facula/mach)) 
-    print(mach - facula * range(31, 4))
+    # reduce the number of results
 
-    result = reverse(step, mach, facula)
+    if mach > facula:
+        diff = mach - facula
+        count = int(round(diff/facula))
+        mach = mach - facula * count
+        result = reverse(count, mach, facula)
+
+    elif facula > mach:
+        diff = facula - mach
+        count = int(round(diff/mach))
+        facula = facula - mach * count
+        result = reverse(count, mach, facula)
+
     if not result:
         return "impossible"
+ 
     return result
 
-def reverse(step, M, F):
-    if M and F == 1:
-        if step == 1:
-            return step 
-        else:
-            return step + 1
-    elif M > F:
-        M = M - F
-        return reverse(step + 1, M, F)
-    elif F > M:
-        F = F - M
-        return reverse(step + 1, F, M)
-    else:
-        return
-            
-answer('2', '1')
+def reverse(count, m, f):
+    if m == 1 and f == 1:
+        return str(count)
+    if is_valid(m, f):
+        if m > f:
+            m = m - f
+            return reverse(count + 1, m, f)
+        elif f > m:
+            f = f - m
+            return reverse(count + 1, m, f)
+     
+def is_valid(m, f):
+    if any(x <= 0 for x in [m, f]):
+        return False
+
+print(answer('2', '4'))
+
